f9502336989784e2879dbb15065285c743b0435c
Paul Jose <s221061447@deakin.edu.au>
2023-04-10 20:25:13 +1000

    Simple microservice application running through Kubernetes

---
+++ users/src/server.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import { logger } from './logger.js';
+import bodyParser from 'body-parser';
+import userApi from './userApi.js';
+
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+   
+// Define Express parameters
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(__dirname + '/public'));
+
+app.use("/users", userApi);
+
+const port = 3030;
+
+// Start Server
+app.listen(port, () => {
+    logger.log({
+        level: 'info',
+        message: `App listening on port: ${port}`,
+    });
+});
